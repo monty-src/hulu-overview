a5f2853c1451cca1a5ec7d7e663b4e8b50746725
monty-src <phoebe@montiers-mbp.lan>
2022-06-30 10:23:54 -0400

    Dev renamed project

--- components/Header.js
+++ components/Header.js
@@ -1,7 +1,13 @@
+import Image from 'next/Image';
 const Header = () => {
   return (
     <header className=''>
       <h1>This is the header</h1>
+      <Image
+        className="object-container"
+        src="https://links.papareact.com/ua6"
+        width={200}
+        height={100}/>
     </header>
   )
 };
