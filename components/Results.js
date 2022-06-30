eb1ea672f804df0980b9730d132c2084eecc9ec1
monty-src <phoebe@montiers-mbp.lan>
2022-06-30 16:12:40 -0400

    Dev Added grid to results page

--- components/Results.js
+++ components/Results.js
@@ -2,10 +2,15 @@ import Thumbnail from './Thumbnail';
 
 const Results = ({ results }) => {
   return (
-    <div className="mt-8">
+    <div className="
+      px-5
+      my-10
+      sm:grid
+      md:grid-cols-2
+      xl:grid-cols-3">
       {results.map(result => (
         <Thumbnail key={result.id} result={result} />
-      ))}b
+      ))}
     </div>
   )
 }
