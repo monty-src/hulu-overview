b865bd61f5ff6de7405ede3d8291abb582cfcd36
monty-src <phoebe@montiers-mbp.lan>
2022-06-30 11:00:52 -0400

    Dev added flex to header and header icons

--- components/Header.js
+++ components/Header.js
@@ -13,12 +13,21 @@ import HeaderItem from './HeaderItem';
 
 const Header = () => {
   return (
-    <header>
-      <div>
+    <header className="flex flex-col">
+      <div className="
+        flex
+        flex-grow
+        justify-evenly
+        max-w-2xl">
         <HeaderItem title='HOME' Icon={HomeIcon} />
+        <HeaderItem title='TRENDING' Icon={LightningBoltIcon} />
+        <HeaderItem title='VERIFIED' Icon={BadgeCheckIcon} />
+        <HeaderItem title='COLLECTIONS' Icon={CollectionIcon} />
+        <HeaderItem title='SEARCH' Icon={SearchIcon} />
+        <HeaderItem title='ACCOUNT' Icon={UserIcon} />
       </div>
       <Image
-        className="object-container"
+        className="object-contain"
         src="https://links.papareact.com/ua6"
         width={200}
         height={100}/>
